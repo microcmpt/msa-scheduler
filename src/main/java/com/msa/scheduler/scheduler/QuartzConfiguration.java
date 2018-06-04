5cc9e1d12f4f1430af0b6dd0ad6842e1391f2b75
xiaping.shen <xiaping.shen@anxincloud.com>
2018-06-04 11:24:17 +0800

    定时任务调度和邮件配置

--- src/main/java/com/msa/scheduler/scheduler/QuartzConfiguration.java
+++ src/main/java/com/msa/scheduler/scheduler/QuartzConfiguration.java
@@ -2,10 +2,9 @@ package com.msa.scheduler.scheduler;
 
 import lombok.extern.slf4j.Slf4j;
 import org.quartz.Scheduler;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.support.PropertiesLoaderUtils;
 import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 
@@ -21,16 +20,6 @@ import java.util.Properties;
 @Configuration
 public class QuartzConfiguration {
     /**
-     * The constant configPathSuffix.
-     */
-    private static final String configPathSuffix = "config/quartz.properties";
-    /**
-     * The Config path prefix.
-     */
-    @Value("${config.path.prefix}")
-    private String configPathPrefix;
-
-    /**
      * Scheduler factory bean scheduler factory bean.
      *
      * @return the scheduler factory bean
@@ -38,17 +27,11 @@ public class QuartzConfiguration {
     @Bean
     public SchedulerFactoryBean schedulerFactoryBean() {
         SchedulerFactoryBean schedulerFactoryBean = new SchedulerFactoryBean();
-        String configPath = "";
         try {
-            if (configPathPrefix.lastIndexOf("/") != -1) {
-                configPath = configPathPrefix + configPathSuffix;
-            } else {
-                configPath = configPathPrefix + "/" + configPathSuffix;
-            }
-            Properties properties = PropertiesLoaderUtils.loadProperties(new FileSystemResource(configPath));
+            Properties properties = PropertiesLoaderUtils.loadProperties(new ClassPathResource("quartz.properties"));
             schedulerFactoryBean.setQuartzProperties(properties);
         } catch (IOException e) {
-            log.error("load {} file error", configPath, e);
+            log.error("load {} file error", "quartz.properties", e);
         }
         return schedulerFactoryBean;
     }
