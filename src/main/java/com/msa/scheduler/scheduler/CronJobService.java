a962bb71f441fb0b1b6060af3c21308b6425079c
xiaping.shen <xiaping.shen@anxincloud.com>
2018-06-15 18:10:06 +0800

    重构定时任务调度任务中心

--- src/main/java/com/msa/scheduler/scheduler/CronJobService.java
+++ src/main/java/com/msa/scheduler/scheduler/CronJobService.java
@@ -59,17 +59,13 @@ public class CronJobService {
             if (jobModule.isStartNow()) {
                 triggerBuilder.startNow();
             }
-            //——不触发立即执行
-            //——等待下次Cron触发频率到达时刻开始按照Cron频率依次执行
+            // 不触发立即执行，等待下次Cron触发频率到达时刻开始按照Cron频率依次执行
             CronScheduleBuilder cronScheduleBuilder = CronScheduleBuilder
                     .cronSchedule(jobModule.getCron()).withMisfireHandlingInstructionDoNothing();
-            //——以错过的第一个频率时间立刻开始执行
-            //——重做错过的所有频率周期后
-            //——当下一次触发频率发生时间大于当前时间后，再按照正常的Cron频率依次执行
+            // 以错过的第一个频率时间立刻开始执行，重做错过的所有频率周期后，当下一次触发频率发生时间大于当前时间后，再按照正常的Cron频率依次执行
             if (Objects.equals(jobModule.getMisfire(), Trigger.MISFIRE_INSTRUCTION_IGNORE_MISFIRE_POLICY)) {
                 cronScheduleBuilder.withMisfireHandlingInstructionIgnoreMisfires();
-                //——以当前时间为触发频率立刻触发一次执行
-                //——然后按照Cron频率依次执行
+            // 以当前时间为触发频率立刻触发一次执行，然后按照Cron频率依次执行
             } else if (Objects.equals(jobModule.getMisfire(), CronTrigger.MISFIRE_INSTRUCTION_FIRE_ONCE_NOW)) {
                 cronScheduleBuilder.withMisfireHandlingInstructionFireAndProceed();
             }
