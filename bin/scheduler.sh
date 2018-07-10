a0996f1ff71f382ea4e87070bc26803226b45952
xiaping.shen <xiaping.shen@anxincloud.com>
2018-07-10 14:37:21 +0800

    添加linux启动脚本

---
+++ bin/scheduler.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+# 启动类
+export MAIN_CLASS=com.msa.schedulercom.msa.scheduler.SchedulerBootstrap
+
+# start scheduler center
+if [ "$1" = "start" ] ; then
+    echo -------------------------------------------
+    echo start scheduler center
+    echo -------------------------------------------
+
+    # 设置项目代码路径
+    export CODE_HOME=".."
+
+    # 设置依赖路径
+    export CLASSPATH="$CODE_HOME/lib/msa-scheduler-1.0.1.jar"
+
+    # java可执行文件位置
+    export _EXECJAVA="$JAVA_HOME/bin/java"
+
+    # 设置配置文件路径
+    export CONFIG_PATH=..
+
+    # JVM启动参数
+    export JAVA_OPTS="-server -Xms128m -Xmx256m -Xss256k -XX:MaxDirectMemorySize=128m -Dconfig.path.prefix=$CONFIG_PATH"
+
+    $_EXECJAVA $JAVA_OPTS -classpath $CLASSPATH $MAIN_CLASS &
+
+# stop scheduler center
+elif [ "$1" = "stop" ] ; then
+    echo -------------------------------------------
+    echo stop scheduler center
+
+    #所有相关进程
+    PIDs=`jps -l | grep $MAIN_CLASS | awk '{print $1}'`
+    #停止进程
+    if [ -n "$PIDs" ]; then
+      for PID in $PIDs; do
+          kill $PID
+          echo "kill $PID"
+      done
+    fi
+
+    #等待50秒
+    for i in 1 10; do
+      PIDs=`jps -l | grep $MAIN_CLASS | awk '{print $1}'`
+      if [ ! -n "$PIDs" ]; then
+        echo "stop scheduler center success"
+        echo -------------------------------------------
+        break
+      fi
+      echo "sleep 5s"
+      sleep 5
+    done
+
+    #如果等待50秒还没有停止完，直接杀掉
+    PIDs=`jps -l | grep $MAIN_CLASS | awk '{print $1}'`
+    if [ -n "$PIDs" ]; then
+      for PID in $PIDs; do
+          kill -9 $PID
+          echo "kill -9 $PID"
+      done
+    fi
+fi
